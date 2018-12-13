5e0dc410d62c819ff8b6652f45b7599a374b12ac
Purinut Uomrod <42512167+gearprn@users.noreply.github.com>
2018-12-13 17:32:57 +0700

    Updata gross.py, gross_chart.svg
    - เปลี่ยนจากการใช้ loop วนเข้าไปเช็ก รายได้ ใน data frame ไปใช้ groupby เเนวหนัง เเละ sum หาผลบวกของรายได้ทั้งหมดของเเต่ละเเนวหนัง
    
    - เพิ่มรายละเอียดในเเต่ละ bar (lable) ว่ารายได้ในเเต่ละเเนวหนังถูกคิดในหลักล้าน

--- python/gross.py
+++ python/gross.py
@@ -8,28 +8,22 @@ import pandas, pygal
 def main():
     """ create chart for gross """
     data_frame = pandas.read_csv("movies.csv", encoding = "utf-8")
+    grouped = data_frame.groupby("genre")
 
     gross_dict = {}
+    for genre, gross_data_frame in grouped:
+        gross_dict[genre] = str(gross_data_frame.sum()[5])
 
-    for i in range(6819):
-        if data_frame["genre"][i] not in gross_dict:
-            gross_dict["%s"%(data_frame["genre"][i])] = int(data_frame["gross"][i])
-        else:
-            gross_dict["%s"%(data_frame["genre"][i])] += int(data_frame["gross"][i])
-
-    gross_dict = dict(sorted(gross_dict.items(), key=lambda x: x[1]))
-
-    for i in gross_dict:
-        gross_dict[i] = str(gross_dict[i])
-        gross_dict[i] = gross_dict[i][:len(gross_dict[i])-6]
-        gross_dict[i] = int(gross_dict[i])
+    gross_dict = dict(sorted(gross_dict.items(), key=lambda x: int(x[1])))
 
     bar_chart = pygal.Bar()
     bar_chart.title = "Total gross in each gross from 1986-2016 (in million)"
+
     for i in gross_dict:
-        bar_chart.add(i, [{"value":gross_dict[i], "lable":"in million" }])
+        bar_chart.add(i, [{"value": int(gross_dict[i][:len(gross_dict[i])-6]), "label": "in million" }])
+
     bar_chart.legend_at_bottom = True
-    bar_chart.legend_box_size = 16
+    bar_chart.legend_box_size = 20
 
     bar_chart.render_to_file('gross_chart.svg')
 
