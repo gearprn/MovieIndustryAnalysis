51a5d3aafca9c0ff3b29e41502b39ebfc2dc9c52
Purinut Uomrod <42512167+gearprn@users.noreply.github.com>
2018-12-13 17:04:00 +0700

    Update genre.py, genre_chart.svg
    - เปลี่ยนจากการใช้ loop วนเข้าไปเช็ก genre ใน data frame ไปใช้ groupby เเทน
    
    - เพิ่มรายละเอียดในเเต่ละ bar ว่าเเนวหนังนั้นคิดเป็นกี่ % จากหนังทั้งหมด

--- python/genre.py
+++ python/genre.py
@@ -8,25 +8,22 @@ import pandas, pygal
 def main():
     """ create chart """
     data_frame = pandas.read_csv("movies.csv", encoding = "utf-8")
+    genre_grouped = data_frame.groupby('genre')
 
-    genre_dict = {}
-    for i in range(6819):
-        if data_frame["genre"][i] not in genre_dict:
-            genre_dict[data_frame["genre"][i]] = 1
-        else:
-            genre_dict[data_frame["genre"][i]] += 1
+    genre_amount = {}
+    for genre, genre_data_frame in genre_grouped:
+        genre_amount[genre] = genre_data_frame.count()[3]
 
-    genre_dict = dict(sorted(genre_dict.items(), key=lambda x: x[1]))
+    genre_amount = dict(sorted(genre_amount.items(), key=lambda x: x[1]))
 
     bar_chart = pygal.Bar()
     bar_chart.title = "Total movies in each genre from 1986 to 2016"
 
-    for i in genre_dict:
-        bar_chart.add(i, genre_dict[i])
+    for i in genre_amount:
+        bar_chart.add(i, [{'value': genre_amount[i], 'label': "%.2f"%(genre_amount[i]/6820*100) + "%"}])
 
     bar_chart.legend_at_bottom = True
-    bar_chart.legend_box_size = 16
-
+    bar_chart.legend_box_size = 20
     bar_chart.render_to_file('genre_chart.svg')
 
 main()
