6426fcbf8019b6a3b3c492545915f390d9310237
Purinut Uomrod <42512167+gearprn@users.noreply.github.com>
2018-12-15 19:44:28 +0700

    Updata country_company.py
    - เปลี่ยนจาก Treemap เป็น Pie กราฟ
    - ใช้ groupby  "country"

--- python/country_company.py
+++ python/country_company.py
@@ -1,20 +1,28 @@
-""" MOVIE ANALYSIS """
+""" 
+    MOVIE ANALYSIS
+    chart : country
+"""
 import pandas, pygal
 def main():
     """ chart: country """
     data_frame = pandas.read_csv("movies.csv", encoding="utf-8")
-    comp_dict = {}
+    country_grouped = data_frame.groupby('country')
 
-    movie_chart = pygal.Treemap()
-    movie_chart.title = "Where most movie from?"
+    country_dict = {}
 
-    for i in range(6819):
-        if data_frame["company"][i] not in comp_dict:
-            comp_dict[data_frame["company"][i]] = 1
-        else:
-            comp_dict[data_frame["company"][i]] += 1
+    for country, country_data_frame in country_grouped:
+        country_dict[country] = country_data_frame.count()[2]
 
-    for i in comp_dict:
-        movie_chart.add(i, comp_dict[i])
-    movie_chart.render_to_file("country_chart.svg")
+    country_dict = dict(sorted(country_dict.items(), key=lambda x :x[1]))
+
+    pie_chart = pygal.Pie()
+    pie_chart.title = "Where most movies from?"
+
+    for i in list(country_dict)[-1:-6:-1]:
+        pie_chart.add(i, [{"value": country_dict[i], "label": "%.2f"%(country_dict[i]/6820*100) + "%"}])
+
+    pie_chart.legend_at_bottom = True
+    pie_chart.legend_box_size = 20
+    pie_chart.render_to_file('country_chart.svg')
+
 main()
