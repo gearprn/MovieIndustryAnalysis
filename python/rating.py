6c297a5a27a1054e5b9c4146ff70b5533d2f3672
Purinut Uomrod <42512167+gearprn@users.noreply.github.com>
2018-12-16 14:53:07 +0700

    Update rating.py, rating_chart.svg, index.html

--- python/rating.py
+++ python/rating.py
@@ -9,9 +9,11 @@ def main():
     """ create chart for genre """
     data_frame = pandas.read_csv("movies.csv", encoding = "utf-8")
 
-    rating_dict = {}
+    rating_dict = {"UNRATED":0}
     for i in range(6819):
-        if data_frame["rating"][i] not in rating_dict:
+        if str(data_frame["rating"][i]) == "NOT RATED" or str(data_frame["rating"][i]) == "Not specified" or str(data_frame["rating"][i]) == "UNRATED":
+            rating_dict["UNRATED"] += 1
+        elif data_frame["rating"][i] not in rating_dict:
             rating_dict[data_frame["rating"][i]] = 1
         else:
             rating_dict[data_frame["rating"][i]] += 1
@@ -22,7 +24,7 @@ def main():
     bar_chart.title = "Total rating from 1986 to 2016"
 
     for i in rating_dict:
-        bar_chart.add(i, [{"value": rating_dict[i], "label": "%.2f"%(rating_dict[i]/6820*100) + "%"}])
+        bar_chart.add(i, [{"value": rating_dict[i], "label": "%.2f"%(rating_dict[i]/6820*100)+"%"}])
 
     bar_chart.legend_at_bottom = True
     bar_chart.legend_box_size = 16
