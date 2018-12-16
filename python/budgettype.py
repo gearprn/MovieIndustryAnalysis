2bf93ace3fc3c248ba5f75f91aee118d7f887889
ploidplanp <waoekpr@gmail.com>
2018-12-16 18:28:29 +0700

    re average budget
    change to horizontal bar
    
    Co-Authored-By: Purinut Uomrod <gearprn@users.noreply.github.com>

--- python/budgettype.py
+++ python/budgettype.py
@@ -7,21 +7,20 @@ def main():
     """ creat chart """
     typ = []
     data_frame = pandas.read_csv("movies.csv", encoding = "utf-8")
-    genre_grouped = data_frame.groupby('genre')
-    budget_data = genre_grouped.mean()
+    genre_grouped = data_frame.groupby('genre').mean()
 
     for i in range(6819):
         if data_frame["genre"][i] not in typ:
             typ.append(data_frame["genre"][i])
     typ = sorted(typ)
     print(typ)
-    print(budget_data)
-    chart = pygal.Bar()
+    print(genre_grouped)
+    chart = pygal.HorizontalBar()
     chart.title = "Average Budget"
 
 
     for i in range(17):
-        chart.add(typ[i], budget_data["budget"][i])
+        chart.add(typ[i], genre_grouped["budget"][i])
 
     chart.render_to_file('avr_budget.svg')
 
